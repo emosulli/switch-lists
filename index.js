ca1874dd7f8c775ec3c652f5245305ec4de2e125
ellie o'sullivan <emosulli@uvm.edu>
2024-10-30 10:29:39 -0400

    Initial Commit

---
+++ index.js
@@ -0,0 +1,12 @@
+//make lists
+const sweetRecipes = ["muffins", "pies", "cookies", "cake"]
+const spicyRecipes = ["noodles", "curry", "spicy cake", "evil cookies"]
+//get elements by id
+const switchList = () => {
+    const sweetIsChecked = document.getElementById('sweet').checked;
+    const spicyIsChecked = document.getElementById('spicy').checked;
+}
+//write if/else
+if (sweetIsChecked) {
+    
+} 
