cc1c4f9c52eca7bce3e395fc6062a987bff85534
ellie o'sullivan <emosulli@uvm.edu>
2024-11-03 17:55:59 -0500

    Second Commit

--- index.js
+++ index.js
@@ -1,12 +1,42 @@
 //make lists
 const sweetRecipes = ["muffins", "pies", "cookies", "cake"]
 const spicyRecipes = ["noodles", "curry", "spicy cake", "evil cookies"]
+
+
 //get elements by id
+const displayRecipes = (list) => {
+    const recipeList = document.getElementById('recipe-list');
+
+    for (index in list) {
+        const food = list[index];
+
+        // create a new list item
+        const newListItem = document.createElement('li');
+
+        // update the text of that new list item to be food
+        newListItem.textContent = food;
+ 
+        // add the new list item to the unordered list on the screen
+        recipeList.appendChild(newListItem);
+    }
+
+}
+const recipeList = document.getElementById('recipe-list');
+
 const switchList = () => {
     const sweetIsChecked = document.getElementById('sweet').checked;
     const spicyIsChecked = document.getElementById('spicy').checked;
 }
+
+recipeList.textContent = '';
+
 //write if/else
 if (sweetIsChecked) {
-    
+    displayRecipes(sweetRecipes);
 } 
+else if (spicyIsChecked) {
+    displayRecipes(spicyRecipes);
+}
+else {
+    recipeList.textContent = 'please choose an option'
+}
